5facb5e79cdd0cb2b01fbb6df1ded08c97e6a2d0
Taylor Ng <taylormng95@gmail.com>
2024-06-10 08:42:14 -0400

    completes POST portion

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:4000/questions')
+      .then(response => response.json())
+      .then(questions => setQuestions(questions))
+  }, [])
+
+  function addQuestion(newQuestion){
+    setQuestions([...questions, newQuestion])
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm addQuestion={addQuestion}/> : <QuestionList questions={questions}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,19 +1,12 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  const [questions, setQuestions] = useState([])
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:4000/questions')
-      .then(response => response.json())
-      .then(questions => setQuestions(questions))
-  }, [])
+function QuestionList({questions}) {
 
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{questions.map(question => <QuestionItem question={question}/>)}</ul>
+      <ul>{questions.map(question => <QuestionItem key={question.id} question={question}/>)}</ul>
     </section>
   );
 }
