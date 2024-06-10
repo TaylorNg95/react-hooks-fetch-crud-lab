71bfff017839ced2ef15b3630825ff2f3362732a
Taylor Ng <taylormng95@gmail.com>
2024-06-10 08:23:17 -0400

    completes GET portion

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:4000/questions')
+      .then(response => response.json())
+      .then(questions => setQuestions(questions))
+  }, [])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map(question => <QuestionItem question={question}/>)}</ul>
     </section>
   );
 }
